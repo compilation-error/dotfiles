049c2e0a2c48c80fd8b324709c0ce7f4cc8e432c
Romeet <romeetc@gmail.com>
2019-01-22 20:38:13 +0000

    Added setup.sh

---
+++ setup.sh
@@ -0,0 +1,6 @@
+ln -s conky/gray.conkyrc $HOME/.conkyrc
+
+ln -s i3/config $HOME/.config/i3/config
+ln -s polybar/config $HOME/.config/polybar/config
+ln -s polybar/launch.sh $HOME/.config/polybar/launch.sh
+
