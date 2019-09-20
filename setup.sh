2714f1938bd7b466bac1ad9f397582a6998fcacf
compilation-error <romeetc@gmail.com>
2019-09-20 17:22:02 -0400

    updated setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,10 @@
-ln -s conky/gray.conkyrc $HOME/.conkyrc
+# setup soft links to config files
+ln -s $PWD/vim/vimrc $HOME/.vimrc
+ln -s $PWD/conky/gray.conkyrc $HOME/.conkyrc
+ln -s $PWD/Xresources/Xresources $HOME/.Xresources
+ln -s $PWD/i3 $HOME/.config/i3
+ln -s $PWD/polybar $HOME/.config/polybar
 
-ln -s i3/config $HOME/.config/i3/config
-ln -s polybar/config $HOME/.config/polybar/config
-ln -s polybar/launch.sh $HOME/.config/polybar/launch.sh
+# install vim-plug
+sh $PWD/vim/vim-setup.sh
 
