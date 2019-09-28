d89934afa292e6270f9e4c85681dabebf3262fda
compilation-error <romeetc@gmail.com>
2019-09-27 23:40:21 -0400

    added nord theme support and switched to it partially

--- setup.sh
+++ setup.sh
@@ -5,7 +5,10 @@ ln -s $PWD/Xresources/Xresources $HOME/.Xresources
 ln -s $PWD/i3 $HOME/.config/i3
 ln -s $PWD/polybar $HOME/.config/polybar
 ln -s $PWD/dunst $HOME/.config/dunst
+ln -s $PWD/tmux/tmux.conf $HOME/.tmux.conf
 
 # install vim-plug and vim plugins
 sh $PWD/vim/vim-setup.sh
 
+# install tpm for tmux plugin
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
