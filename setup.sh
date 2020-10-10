82ce390782900d1ec6adf787020ee94e317c5515
compilation-error <romeetc@gmail.com>
2020-10-10 18:58:05 -0400

    conky updates and powerlevel10k auto download

--- setup.sh
+++ setup.sh
@@ -18,3 +18,5 @@ git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
 # install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+
